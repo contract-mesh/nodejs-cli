c9f3d82e137b8b99671c63093d9d1aca38503327
Tony Chow <tony.chow@elliemae.com>
2020-06-19 16:21:30 -0700

    Deleting js files

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cli = require("./src/cli");
-cli.run('root').catch(function (err) { return console.error(err); });
-//# sourceMappingURL=index.js.map
